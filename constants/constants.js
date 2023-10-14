6cf0c16ef00f180889e9467470c7ad2d3198836b
Sonu Bhagat <68046859+SonuBhagat@users.noreply.github.com>
2023-10-14 23:37:22 +0530

    Add files via upload

---
+++ constants/constants.js
@@ -0,0 +1,64 @@
+export const NAME = 'Sonu Bhagat';
+
+export const DESIGNATION = `Digital Marketing Strategist | Google Ads | Facebook Ads | SEO & SEM Expert | Graphic Designer`;
+
+export const DETAILS = {
+  Residence: 'India ',
+  City: 'Rudrapur',
+  State: 'Uttrakhand',
+  Age: '23',
+};
+
+export const SKILLS = [
+  {
+    title: 'MERN Stack Developer',
+    level: '85%',
+  },
+  {
+    title: 'React Developer',
+    level: '90%',
+  },
+  {
+    title: 'Desktop Application Developer',
+    level: '75%',
+  },
+  {
+    title: 'Backend Developer',
+    level: '55%',
+  },
+  {
+    title: 'Technical Blogger',
+    level: '80%',
+  },
+];
+
+export const TECH_STACK = [
+  'SEO',
+  'Content Marketing',
+  'Social Media',
+  'Email Marketing',
+'PPC Advertising',
+'Data Analytics',
+'Marketing Automation',
+'Graphic Design',
+'Video Marketing',
+'Web Development',
+'Copywriting',
+'Mobile Marketing',
+'E-commerce',
+'Testing & Optimization',
+'Market Research',
+'Budgeting & ROI Analysis',
+'Adaptability & Learning',
+];
+
+export const CONTACTS = {
+  EMAIL: 'sonu.bhagatx@gmail.com',
+  PHONE: '+91 8439171577',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/SonuBhagat/',
+  LINKEDIN: 'https://www.linkedin.com/in/sonubhagat/',
+  TWITTER: 'https://twitter.com/',
+  FACEBOOK: 'https://www.facebook.com/',
+};
